bc47dd5b6a5c1706da424a6bf58e7e64f3e5ea0f
Elkiscoming <Elkiscoming@gmail.com>
2016-02-14 23:39:49 +0330

    Compile task now works.

--- AIC_runner/base/tasks.py
+++ AIC_runner/base/tasks.py
@@ -1,8 +1,38 @@
 from celery import shared_task
 from .models import Submit
+from docker_sandboxer.sandboxer import Parser, Sandbox
+from docker_sandboxer.scheduler import CPUScheduler
+from AIC_runner import settings
+import os
+import json
 
 
 @shared_task(bind=True, queue='compile_queue')
 def compile_code(self, submit_id):
-    print("Compiling %d" % submit_id)
+    submit = Submit.objects.get(pk=submit_id)
+    submit.status = 1
+    sandbox = Sandbox()
+    sandbox.update_limits(cpu=[1024])
+
+    cpu_scheduler = CPUScheduler()
+    parser = Parser(cpu_scheduler, settings.COMPILE_DOCKER_YML_ROOT, settings.COMPILE_DOCKER_YML_LOG_ROOT)
+
+    submit_code = os.path.join(settings.MEDIA_ROOT, str(submit.code))
+    compile_context = {
+        'code_image': submit.lang.compile_container.get_image_id(),
+        'code_zip': submit_code,
+        'code_log': submit_code + '_log',
+        'code_compile': submit_code + '_compiled',
+    }
+
+    parser.create_yml_and_run(str(submit_id), "compile.yml", compile_context)
+
+    with open(compile_context['code_log'] + '/status.log') as data_file:
+        data = json.load(data_file)
+
+    submit.status = 2 if data['compile_success'] else 3
+    submit.compile_log_file = '' if 'compile_error' not in data else data['compile_error']
+    submit.compiled_code = compile_context['code_compile'] + '/compiled.zip'
+    submit.save()
+    print("Compile end %s" % data)
 
